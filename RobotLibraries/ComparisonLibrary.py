7901e119c55048522e1011c0bceac85cacb28357
Sophie Tual <sophie.tual@ledger.fr>
2021-09-09 12:58:55 +0200

    Making request and comparison libs more resilient

--- RobotLibraries/ComparisonLibrary.py
+++ RobotLibraries/ComparisonLibrary.py
@@ -1,19 +1,35 @@
-from deepdiff import DeepDiff, DeepSearch
+from deepdiff import DeepDiff
 from robot.api.deco import keyword, library
 from robot.api import logger
 from pprint import pprint
 
+
 @library
 class ComparisonLibrary:
 
-    @keyword('Compare Two Payloads')
+    @keyword("Compare Two Payloads")
     def compare_payloads(self, old_payload, new_payload):
+
         ddiff = DeepDiff(old_payload, new_payload, ignore_order=True)
-        if ddiff:
-            try:
-                logger.error("Output : " + pprint(str(ddiff.to_json()), indent=2))
-            except Exception as e:
-                print(e)
-            raise ValueError('Payloads do not match!')
-        return ddiff
+        if "get" in [*old_payload.values()]:
+            if ddiff:
+                try:
+                    logger.error(pprint(str(ddiff.to_json()), indent=2))
+                except Exception as e:
+                    print(e)
+                raise ValueError("Payloads do not match!")
+            logger.info("Payloads match")
+            return ddiff
+        # Obviously for POST calls, values (id, date) will be different
+        # We want to ignore the differences and look only at type changes
+        if "post" in [*old_payload.values()]:
+            if "type_changes" in ddiff:
+                try:
+                    logger.error(pprint(str(ddiff.to_json()), indent=2))
+                except Exception as e:
+                    print(e)
+                raise ValueError("Payloads do not match!")
+            logger.info("Payloads match")
+            return ddiff
+
 
--- RobotLibraries/RequestsHelperLibrary.py
+++ RobotLibraries/RequestsHelperLibrary.py
@@ -2,18 +2,22 @@ import requests
 import json
 from robot.api.deco import keyword, library
 from robot.api import logger
+import os
 
 
 @library
 class RequestsHelperLibrary:
 
-    @keyword('Populate Payload With ')
+    @keyword("Populate Payload With ")
     def populate_payload(self, data_json, test_path):
-        query_params = self._return_corresponding_payload_for_uri(test_path, data_json)
+        query_params = self._return_corresponding_payload_for_uri(data_json, test_path)
         return query_params
 
-    @keyword('Fetch Payload From ')
+    @keyword("Fetch Payload From ")
     def get_payload(self, endpoint, query_object):
+        # Getting auth token from environment variables
+        headers = {"Authorization": os.environ['APPSTORE_AUTH'],
+                   "Content-Type": "application/json"}
         # Formatting the url for variables
         if "uri_params" in query_object:
             # "*" allows us to iterate through an array. Neat little trick ;)
@@ -21,22 +25,29 @@ class RequestsHelperLibrary:
         else:
             uri = query_object["uri"]
         if query_object["method"] == "post":
+            data = json.dumps(query_object["payload"])
             response = requests.post(
                 url=endpoint+uri+query_object["query_string_params"],
-                data=query_object["payload"]
+                headers=headers,
+                data=data
             )
             logger.info("Call on endpoint : " + uri)
+            logger.info(response.text)
         if query_object["method"] == "get":
-            response = requests.get(url=endpoint+uri, timeout=None)
+            response = requests.get(url=endpoint+uri,
+                                    timeout=None)
             logger.info("Call on endpoint : " + uri)
+            logger.info(response.text)
         # if the response_code is any of 4**, 5**
         if not str(response.status_code).startswith('20'):
             logger.error("Output : " + response.text)
-            raise ValueError('Could not perform the request on endpoint ' + endpoint)
-        return response.json()
+            raise ValueError("Could not perform the request on endpoint " + endpoint)
+        response = dict(response.json())
+        response["call_method"] = query_object["method"]
+        return response
 
-    def _return_corresponding_payload_for_uri(self, test_path, data_json):
-        file = open(test_path + '/data.json')
+    def _return_corresponding_payload_for_uri(self, data_json, test_path):
+        file = open(test_path + "/data.json")
         # returns JSON object as a dict
         data = json.load(file)
         return data[data_json]
